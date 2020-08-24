69a2339b09b30197c3d6dd953be51082d1cd2e78
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-24 16:31:23 -0500

    Add project files.

---
+++ InterfaceExamples/Duck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExamples
+{
+    class Duck : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Quack, I'm a flying duck");     
+        }
+    }
+    class Airplane : IFlyable, ITakeOff
+    {
+        public void Fly()
+        {
+            Console.WriteLine("We're taking off on a plane");
+        }
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"Taking off to go to {destination}");
+        }
+    }
+
+    class Helicopter : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("hovering in a helicopter");
+        }
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"Taking off to go to {destination}");
+        }
+    }
+    //public by default, cannot declare any actual behavior for methods, they are just definitions of method signatures
+    // classes that use this interface must have this kind of method
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    // also can add IFlyable to the ITakeOff to implement both when ITakeOff is called
+    interface ITakeOff : IFlyable
+    {
+        void TakeOff(string destination);
+    }
+    
+}
---
+++ InterfaceExamples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            var duck = new Duck();
+            var plane = new Airplane();
+            var helicopter = new Helicopter();
+
+            var flyingThings = new List<IFlyable> { duck, plane, helicopter };
+
+            foreach (var thing in flyingThings)
+            {
+                // check to see if this thing implement this interface
+                // if so add it to takeoff so that can be messed with
+
+                if (thing is ITakeOff takeOff)
+                {
+                    takeOff.TakeOff("Bahamas");
+                }
+                thing.Fly();
+                
+            }
+        }
+    }
+}
